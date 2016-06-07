6a0573d5bdd88b2b9a0e98288420134d1d3d541c
Jason Smith <jsmith311@gmail.com>
2016-06-06 19:08:57 -0700

    Basic Program Complete

--- Lab1A/Program.cs
+++ Lab1A/Program.cs
@@ -10,6 +10,15 @@ namespace Lab1A
     {
         static void Main(string[] args)
         {
+            string name;
+            string age;
+            System.Console.Write("What is your name? ");
+            name = System.Console.ReadLine();
+            System.Console.Write("What is your age? ");
+            age = System.Console.ReadLine();
+            System.Console.WriteLine("Welcome, "+name+", age "+age+".");
+            System.Console.Write("Press any key to quit.");
+            System.Console.ReadKey();
         }
     }
 }
